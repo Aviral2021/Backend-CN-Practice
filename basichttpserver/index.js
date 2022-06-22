f310e01a3d26b866d29f6b86ec4c596207948619
aviral <aviral.ece.ipu@gmail.com>
2022-06-22 12:46:17 +0530

    ntial commit

---
+++ basichttpserver/index.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const port = 8000;
+const fs = require('fs');
+
+ function requestHandler(req, res){
+     console.log(req.url);
+     res.writeHead(200, {'content-type': 'text/html'});
+
+    fs.readFile('./index.html', function (err, data) {
+        if(err){
+            console.log('error');
+            return res.end('<h1>Error</h1>');
+
+        }
+        return res.end(data);
+        
+    });
+
+    // res.end('Gotcha!');
+ }
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, function(err){
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log("server  is up and running on port", port);
+})
